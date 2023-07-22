3ddbafe625539f380d80041b203ac836a7e7c938
brandonwernham <brandonwern01@gmail.com>
2023-07-22 11:36:18 -0400

    Deployment 9

--- app/settings.py
+++ app/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = "postgresql://postgres:tPglgORt0VqfbsYQVICW@containers-us-west-86.railway.app:5657/railway"
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,10 +88,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
