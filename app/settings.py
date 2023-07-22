d211acd44096bfb230e56877f1a145bc6b6592d2
brandonwernham <brandonwern01@gmail.com>
2023-07-22 13:09:15 -0400

    Deployment 15

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-ryxjaiigz1&d3=6_$avm^o-+g4gh5ut1gg*et!j*)3m_#kosf4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
